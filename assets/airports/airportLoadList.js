def270e714ec26bdc9340408115a6c4e08e101d3
Hawaiicoder <58634283+Hawaiicoder@users.noreply.github.com>
2020-08-20 09:38:29 -0400

    Update airportLoadList.js

--- assets/airports/airportLoadList.js
+++ assets/airports/airportLoadList.js
@@ -211,6 +211,12 @@ window.AIRPORT_LOAD_LIST = (function() { // eslint-disable-line wrap-iife
             premium: false
         },
         {
+            icao: 'kbwi',
+            level: 'hard', 
+            name: 'Baltimore-Washington International Thurgood Marshall Airport',
+            premium: false
+        },
+        {
             icao: 'kcvg',
             level: 'beginner',
             name: 'Cincinnati-Northern Kentucky International Airport',
