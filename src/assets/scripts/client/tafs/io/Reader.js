6515dd4bcc7983139cb0bc0f90c7769b08d220a7
parkerqueen <sobanr4@gmail.com>
2021-03-15 17:45:19 +0500

    Added SID retrieval method

--- src/assets/scripts/client/tafs/io/Reader.js
+++ src/assets/scripts/client/tafs/io/Reader.js
@@ -1,9 +1,23 @@
+import { FLIGHT_PHASE } from "../../constants/aircraftConstants";
+import { PROCEDURE_TYPE } from "../../constants/routeConstants";
+import NavigationLibrary from "../../navigationLibrary/NavigationLibrary";
+
 export default class Reader {
     constructor(app_controller) {
         this.app_controller = app_controller;
     }
 
-    get_all_aircraft_models() {
+    get_all_sids() {
+        return NavigationLibrary.getProceduresByType(PROCEDURE_TYPE.SID);
+    }
+
+    get_all_aircrafts() {
         return this.app_controller.aircraftController.aircraft.list;
+    }
+
+    get_departure_aircrafts() {
+        return this.get_all_aircrafts().filter(
+            (aircraft) => aircraft.flightPhase === FLIGHT_PHASE.APRON
+        );
     }
 }
